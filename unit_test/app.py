08b71adb5ad9e084e58f6a9a4e9fe80b271cea9b
MiracleYoung <miracleyoung0723@gmail.com>
2019-02-20 06:45:20 +0800

    add imdbtop250

--- unit_test/app.py
+++ unit_test/app.py
@@ -5,4 +5,28 @@
 # @File    : app.py
 
 
-print(__name__)
+import requests
+
+headers = {
+    "content-type": "application/json",
+    "Host": "tapdata-server",
+    "Origin": "http://tapdata-server",
+    "Referer": "http://tapdata-server/login",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36",
+}
+
+payload = {
+    "username": "arthur@tapdata.io",
+    "password": "123456",
+    "options": {
+        "device": {"platform": "chrome", "platformVersion": "72.0.3626", "sdkVersion": "4.1.1"}
+    }
+}
+
+res = requests.request(
+    method='POST',
+    url='http://tapdata-server/api/client/v2.0/app/mongodb-charts-jgejk/auth/providers/local-userpass/login',
+    data=payload,
+    headers=headers
+)
+print(res)
